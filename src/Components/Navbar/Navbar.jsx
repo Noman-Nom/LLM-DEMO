9dc58c38c6c6ebf042990488c715df6e0725ecb4
Noman-Nom <noman5456318@gmail.com>
2023-12-08 19:52:05 +0500

    navbar working

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
 import './Navbar.scss'
+import { images } from '../../constants'
+
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='nav'>
 
-        Navbar
-      
+      <div className="nav__left">
+        <div className="nav__logo">
+          <img src={images.logo} alt="" />
+
+        </div>
+
+        <div className="nav__mid">
+          <ul>
+            <li><a href="#">Platform</a></li>
+            <li><a href="#">Companey</a></li>
+            <li><a href="#">Modules</a></li>
+            <li><a href="#">Solutions</a></li>
+            <li><a href="#">Resources</a></li>
+            
+
+          </ul>
+
+          
+        </div>
+
+        <div className="nav__right">
+          <div className="nav__right-login">
+            <button>Login</button>
+          </div>
+
+          <div className="nav__right-btns">
+            <button>Request a Demo</button>
+            <button>Sign up</button>
+          </div>
+
+        </div>
+      </div>
+
+
+
     </div>
   )
 }
