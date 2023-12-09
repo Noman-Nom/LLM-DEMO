481432578a6e4daf3fb3f32da02c727d1665e735
Noman-Nom <noman5456318@gmail.com>
2023-12-10 03:13:18 +0500

    header section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Header.scss'
 import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
+import { RiSingleQuotesL } from "react-icons/ri";
+import { RiSingleQuotesR } from "react-icons/ri";
 
 
 const Header = () => {
@@ -10,12 +12,12 @@ const Header = () => {
             <div className="left">
                 <div className="left__content">
                     <h1><FaQuoteLeft /> Start learning from best <span> platform </span> <FaQuoteRight /></h1>
-                    <p>Study any topic, anytime. explore thousands of courses for the lowest price ever!</p>
+                    <p> <RiSingleQuotesL/> Study any topic, anytime. explore thousands of courses for the lowest price ever! <RiSingleQuotesR/></p>
                 </div>
 
                 <div className="left__form">
                     <div className="form">
-                        <input type="text" />
+                        <input placeholder='what do you want to learn' type="text" />
                         <button><FaSearch/> Search  </button>
                     </div>
                 </div>
