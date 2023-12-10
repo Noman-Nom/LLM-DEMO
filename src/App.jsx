7a531e449b522da63450f59eeb6e898a23f3fdd0
Noman-Nom <noman5456318@gmail.com>
2023-12-10 12:45:59 +0500

    header section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Home from './Pages/Home/Home'
 import './App.css'
+import VideoPage from './Pages/VideoPage/VideoPage'
 
 const App = () => {
   return (
     <div>
         <Home/>
+        <VideoPage/>
     </div>
-  )
+  )     
 }
 
 export default App
---
+++ src/Pages/VideoPage/VideoPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './VideoPage.scss'
+import VideoSection from '../../Components/VideoSection/VideoSection'
+
+const VideoPage = () => {
+  return (
+    <div>
+      {/* hello */}
+      <VideoSection/>
+    </div>
+  )
+}
+
+export default VideoPage
