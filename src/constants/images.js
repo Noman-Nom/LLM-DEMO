9c7a84fa970ef2ef8957277b2187d3a55530a83c
Noman-Nom <noman5456318@gmail.com>
2023-12-10 11:34:32 +0500

    header section completed

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,12 @@
 import logo from '../assets/logo.png'
 import logo1 from '../assets/nomLogo2.png'
+import headerImg from '../assets/headerImg.webp'
 
 
 export default{
     logo,
     logo1,
+    headerImg,
+    
     
 }
