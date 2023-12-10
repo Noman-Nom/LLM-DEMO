126d573fb758d0633736513b65992f7adf1019ef
Noman-Nom <noman5456318@gmail.com>
2023-12-11 02:38:11 +0500

    upcomming course page

---
+++ src/Components/UpCommingCourse/UpCommingCourse.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './UpCommingCourse.scss'
+
+const UpCommingCourse = () => {
+  return (
+    <div>UpCommingCourse</div>
+  )
+}
+
+export default UpCommingCourse
---
+++ src/Pages/UpCommingCoursePage/UpCommingCoursePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './UpCommingCoursePage.scss'
+import UpCommingCourse from '../../Components/UpCommingCourse/UpCommingCourse'
+
+const UpCommingCoursePage = () => {
+  return (
+    <div><UpCommingCourse/></div>
+  )
+}
+
+export default UpCommingCoursePage
