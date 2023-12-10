e56294471be3b0db6aa6823fa6098d4757cc973d
Noman-Nom <noman5456318@gmail.com>
2023-12-11 01:03:03 +0500

    module page working

---
+++ src/Components/Modules/Modules.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Modules.scss'
+
+const Modules = () => {
+  return (
+    <div>Modules</div>
+  )
+}
+
+export default Modules
---
+++ src/Pages/ModulesPage/ModulesPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ModulesPage.scss'
+
+const ModulesPage = () => {
+  return (
+    <div>ModulesPage</div>
+  )
+}
+
+export default ModulesPage
