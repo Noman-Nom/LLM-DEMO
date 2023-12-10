91d034aa98bec8aee87eab5e3d652ef62226031c
Noman-Nom <noman5456318@gmail.com>
2023-12-11 00:10:11 +0500

    black page working

---
+++ src/Pages/BlackPage/BlackPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import BlackDoc from '../../Components/BlackDoc/BlackDoc'
+import './BlackPage.scss'
+
+
+const BlackPage = () => {
+  return (
+    <div>
+        <BlackDoc/>
+    </div>
+  )
+}
+
+export default BlackPage
